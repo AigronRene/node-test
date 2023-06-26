82cf59e93e72c2f03071aed919dd19edbddc2bf5
aigronrene <rene.aigron@gmail.com>
2023-06-26 11:50:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 8081;
+
+// This responds 'Hello World' on the homepage
+
+app.get('/', function (req, res) {
+  res.send('je suis en prod')
+})
+
+// Start server
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
+
+
